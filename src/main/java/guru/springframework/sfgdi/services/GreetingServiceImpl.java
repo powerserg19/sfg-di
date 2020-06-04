12fa9dfe1d436ec9b2847f9eec63ec607190ee3a
sergio <powerserg19@hotmail.com>
2020-06-03 21:05:18 -0400

    new files for lesson 34

---
+++ src/main/java/guru/springframework/sfgdi/services/GreetingServiceImpl.java
@@ -0,0 +1,4 @@
+package guru.springframework.sfgdi.services;
+
+public class GreetingServiceImpl {
+}
---
+++ src/test/java/guru/springframework/sfgdi/controllers/PropertInjectedControllerTest.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgdi.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PropertInjectedControllerTest {
+
+}
