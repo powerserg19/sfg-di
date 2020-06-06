bb1da5157346d21c266bf95d47e8df7031c2ab05
sergio <powerserg19@hotmail.com>
2020-06-06 07:25:30 -0400

    test

--- src/main/java/guru/springframework/sfgdi/services/PropertyInjectedGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/PropertyInjectedGreetingService.java
@@ -3,7 +3,6 @@ package guru.springframework.sfgdi.services;
 import org.springframework.stereotype.Service;
 
 /**
- * Created by jt on 12/27/19.
  */
 @Service
 public class PropertyInjectedGreetingService implements GreetingService {
